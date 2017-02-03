b18011fae8fb805bdf688bdf3c59275532f4cb66
aong <yxwzaxns@gmail.com>
2017-02-04 02:23:44 +0800

    add log output info

--- web.rb
+++ web.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'json'
 require 'net/http'
 
+
 module Sinatra
   class Base
     set :server, %w[thin mongrel webrick]
@@ -10,12 +11,21 @@ module Sinatra
     set :views, File.dirname(__FILE__) + '/views'
     set :environment, :production
     set :logging, true
+    set :root, File.dirname(__FILE__)
+
+    configure :production, :development do
+        enable :logging
+        file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+        file.sync = true
+        use Rack::CommonLogger, file
+      end
   end
 end
 
 # enable :sessions
 
 get '/' do
+  logger.info('Ip: ' + request.ip + ' --Time: ' + Time.new.to_s + ' --Host: ' + request.host)
   uri = URI('https://app.arukas.io/api/containers/0aa14a1e-d23a-44ce-964b-6a59bc5822d4')
 
   req = Net::HTTP::Get.new(uri)
